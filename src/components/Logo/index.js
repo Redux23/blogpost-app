bd26c730349722c4362e6f54ef83b4d0f4b78e6b
Redux23 <bo.osuagwu@mail.comclear$ clear>
2020-06-25 14:52:08 +0100

    Created Logo, Card, and Navbar components.

---
+++ src/components/Logo/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css';
+
+function Logo() {
+    return (
+        <div  className="logo">
+           <a href="#">Bright Osuagwu</a> 
+        </div>
+    )
+}
+
+export default Logo;
