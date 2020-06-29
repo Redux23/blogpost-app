ca2805805ad88f99fc91b1f866a1528784057a42
Redux23 <bo.osuagwu@mail.comclear$ clear>
2020-06-29 21:37:17 +0100

    Added a route for the page components.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import './App.css';
 import Home from './containers/Home';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import ContactUs from './containers/ContactUs';
+import Post from './containers/Post';
 
 
 
 function App() {
   return (
+
+    <Router>
+
     <div className="App">
       <Header/>
       <Hero/>
-      <Home/>
+
+      <Route path="/" exact component={Home} />
+      <Route path="/contact-us" component={ContactUs} />
+      <Route path="/post" component={Post} />
     </div>
+
+    </Router>
   );
 }
 
