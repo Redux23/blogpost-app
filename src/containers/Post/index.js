f67b234dcc5077c3a730d0a6634e08faeaf32a5b
Redux23 <bo.osuagwu@mail.comclear$ clear>
2020-07-17 11:56:56 +0100

    Minor changes made

--- src/containers/Post/index.js
+++ src/containers/Post/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './style.css';
 import Card from '../../components/UI/Card';
-import { BrowserRouter } from 'react-router-dom';
 import BlogPost from '../../components/BlogPost';
 import Sidebar from '../../components/Sidebar';
 
 
-function Post(){
+function Post(props){
+    console.log(props)
+
     return(
 <section className="container">
-<BlogPost />
+<BlogPost {...props}/>
 <Sidebar />
 </section>
         )
