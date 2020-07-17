459d274a38b40393f70eb2d95386234d8f83a7c4
Redux23 <bo.osuagwu@mail.comclear$ clear>
2020-07-17 11:57:40 +0100

    Minor changes made

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
       <Route path="/" exact component={Home} />
       <Route path="/contact-us" component={ContactUs} />
-      <Route path="/post" component={Post} />
+      <Route path="/post/:postId" component={Post} />
     </div>
 
     </Router>
